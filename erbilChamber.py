3806fe32936a2adabca0956a194479e497b32036
Farshad Ghorbani <farshad@Farshads-iMac.local>
2019-08-21 00:30:18 +0300

    VIVA Erbil chmber ended

---
+++ erbilChamber.py
@@ -0,0 +1,31 @@
+import requests
+import bs4
+import re
+import logging
+import urllib.parse
+import csv
+
+
+preURL = "https://www.erbilchamber.org"
+csvFile = open('ErbilChamber.csv', 'w')
+csvWriter = csv.writer(csvFile)
+csvWriter.writerow(['ManagingDirector','CompanyActivity','RegistrationNumber','DateOfRegistration','MobileNumber','Address','NameOfShareholders'])
+
+for i in range(0, 8620, 20):
+    page = requests.get("http://erbilchamber.org/en/companies.html?start="+str(i))
+    soup = bs4.BeautifulSoup(page.content, 'lxml')
+    for companiesLink in soup.find_all('h3', class_="catItemTitle"):
+        companiesURL = preURL + urllib.parse.quote(companiesLink.a['href'])
+
+        companiesPage = requests.get(companiesURL)
+        companiesSoup = bs4.BeautifulSoup(companiesPage.content, 'lxml')
+        csvArray = ['','','','','','','']
+        i = 0
+        for companiesInfo in companiesSoup.find_all('span', class_="itemExtraFieldsValue"):
+            print (companiesInfo.text)
+            i += 1
+            csvArray[i] = companiesInfo.text
+
+        csvWriter.writerow(csvArray)
+
+csvFile.close()
