36dd40665038f2e16d6bab67965b68f32bfe4c27
Farshad Ghorbani <farshad@Farshads-iMac.local>
2019-08-21 15:15:21 +0300

    ended and csv exportd added

--- erbilChamber.py
+++ erbilChamber.py
@@ -1,31 +1,29 @@
 import requests
 import bs4
-import re
-import logging
 import urllib.parse
 import csv
 
 
 preURL = "https://www.erbilchamber.org"
-csvFile = open('ErbilChamber.csv', 'w')
+csvFile = open('ErbilChamber.csv', 'a')
 csvWriter = csv.writer(csvFile)
 csvWriter.writerow(['ManagingDirector','CompanyActivity','RegistrationNumber','DateOfRegistration','MobileNumber','Address','NameOfShareholders'])
+try:
+    for i in range(0, 8620, 20):
+        page = requests.get("http://erbilchamber.org/en/companies.html?start="+str(i))
+        soup = bs4.BeautifulSoup(page.content, 'lxml')
+        for companiesLink in soup.find_all('h3', class_="catItemTitle"):
+            companiesURL = preURL + urllib.parse.quote(companiesLink.a['href'])
 
-for i in range(0, 8620, 20):
-    page = requests.get("http://erbilchamber.org/en/companies.html?start="+str(i))
-    soup = bs4.BeautifulSoup(page.content, 'lxml')
-    for companiesLink in soup.find_all('h3', class_="catItemTitle"):
-        companiesURL = preURL + urllib.parse.quote(companiesLink.a['href'])
-
-        companiesPage = requests.get(companiesURL)
-        companiesSoup = bs4.BeautifulSoup(companiesPage.content, 'lxml')
-        csvArray = ['','','','','','','']
-        i = 0
-        for companiesInfo in companiesSoup.find_all('span', class_="itemExtraFieldsValue"):
-            print (companiesInfo.text)
-            i += 1
-            csvArray[i] = companiesInfo.text
-
-        csvWriter.writerow(csvArray)
-
+            companiesPage = requests.get(companiesURL)
+            companiesSoup = bs4.BeautifulSoup(companiesPage.content, 'lxml')
+            csvArray = ['','','','','','','']
+            j = 0
+            for companiesInfo in companiesSoup.find_all('span', class_="itemExtraFieldsValue"):
+                print (companiesInfo.text)
+                csvArray[j] = companiesInfo.text
+                j += 1
+            csvWriter.writerow(csvArray)
+except:        
+    print(i)
 csvFile.close()
